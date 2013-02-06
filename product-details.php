a075fadde08b055e43bff31c4637fd1dca33350c
Yedukondalu88 <edukondalu@vegaatechnologies.com>
2013-02-06 19:52:09 +0530

    Signed-off-by: Yedukondalu88 <edukondalu@vegaatechnologies.com>

--- product-details.php
+++ product-details.php
@@ -1,11 +1,55 @@
+<?php 
+include 'common.php';
+$product_id=$_GET['product_id'];
+$products="select * from products where product_status=1 and id=".$product_id;
+$product_data = $db->getRow($products);
+print_r($product_data);
+exit;
+// Meta description
+//$Meta_description='';
+//$Meta_keywords='';
+//$Meta_title='';
+if(!empty($product_data['product_meta_description'])) {
+$Meta_description=$product_data['product_meta_description']; 
+}
+else{ 
+$Meta_description=substr($product_data['product_description'], 0, 300);
+}
+// End of Meta description
+
+// Meta Keywords
+if(!empty($product_data['product_meta_keywords'])) {
+$Meta_keywords=$product_data['product_meta_keywords']; 
+}
+else{ 
+$Meta_keywords=substr($product_data['product_title'], 0, 300);
+}
+// End of Meta Keywords
+
+// Meta Title
+if(!empty($product_data['product_meta_title'])) {
+$Meta_title=$product_data['product_meta_title']; 
+}
+else{ 
+$Meta_title=substr($product_data['product_title'], 0, 300);
+}
+// End of Meta Title
+
+//$Meta_description=$product_data['product_meta_description'];
+//$Meta_keywords=$product_data['product_meta_keywords'];
+//$Meta_title=$product_data['product_meta_title'];
+//echo $product_id;
+//exit;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 
 <head>
 
-<title>iFortune</title>
-<meta name="description" content="ifortune">
-<meta name="keywords" content="ifortune">
+<title><?php echo $Meta_description;?></title>
+
+<meta name="description" content="<?php echo $Meta_description;?>">
+<meta name="keywords" content="<?php echo $Meta_description;?>">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="author" content="iFortune" />
 
